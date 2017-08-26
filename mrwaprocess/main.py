57404c7734e1501fd5eb18c6dd6ec383642be098
Sam Spilsbury <smspillaz@gmail.com>
2017-08-26 17:28:03 +0800

    mrwa: Extract both images and subtitles now

--- mrwaprocess/main.py
+++ mrwaprocess/main.py
@@ -16,6 +16,18 @@ from contextlib import contextmanager
 
 
 @contextmanager
+def temporary_directory(**kwargs):
+    """Do something in a temporary directory, yielding it."""
+    tempdir = tempfile.mkdtemp(**kwargs)
+
+    try:
+        yield tempdir
+    finally:
+        pass
+        # shutil.rmtree(tempdir)
+
+
+@contextmanager
 def ffmpeg_decompose_video(video_file):
     """Use ffmpeg to break down a video file into its frames.
 
@@ -23,15 +35,41 @@ def ffmpeg_decompose_video(video_file):
     relative to the present working directory. This context manager
     yields that directory.
     """
-    tempdir = tempfile.mkdtemp(prefix="decomposed", dir=os.getcwd())
-    abs_video_file = os.path.abspath(video_file)
-    subprocess.check_call(["ffmpeg", "-i", abs_video_file, "%04d.jpg"],
-                          cwd=tempdir)
-    try:
-        yield tempdir
-    finally:
-        pass
-        #shutil.rmtree(tempdir)
+    with temporary_directory(prefix="decomposed", dir=os.getcwd()) as tempdir:
+        abs_video_file = os.path.abspath(video_file)
+        subprocess.check_call(["ffmpeg", "-i", abs_video_file, "%04d.jpg"],
+                              cwd=tempdir)
+        try:
+            yield tempdir
+        finally:
+            pass
+            #shutil.rmtree(tempdir)
+
+
+@contextmanager
+def ffmpeg_decompose_srt(video_file):
+    """Use ffmpeg to extract a subtitle file from a video file
+
+    The frames themselves are stored in a temporary directory
+    relative to the present working directory. This context manager
+    yields that subtitles file.
+    """
+    with temporary_directory(prefix="subtitle", dir=os.getcwd()) as tempdir:
+        abs_video_file = os.path.abspath(video_file)
+        subprocess.check_call([
+            "ffmpeg",
+            "-i",
+            abs_video_file,
+            "-map",
+            "0:s:0",
+            "subs.srt",
+            "%04d.jpg"
+        ], cwd=tempdir)
+        try:
+            yield os.path.join(tempdir, "subs.srt")
+        finally:
+            pass
+            #shutil.rmtree(tempdir)
 
 
 def main(argv=None):
@@ -45,4 +83,6 @@ def main(argv=None):
 
     for video in result.videos:
         with ffmpeg_decompose_video(video) as images_directory:
-            print("Images directory: {}".format(images_directory))
+            with ffmpeg_decompose_srt(video) as subtitles_filename:
+                print("Images directory: {}".format(images_directory))
+                print("Subtitles filename: {}".format(subtitles_filename))
